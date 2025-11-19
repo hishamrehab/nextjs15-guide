cc25a2adb88507e5215c29b5fcb608a006abdd20
hishamrehab <hishamrehab30@gmail.com>
2025-11-20 00:28:50 +0200

    Organize project components

---
+++ 1-food-app/components/header.js
@@ -0,0 +1,8 @@
+export default function Header() {
+    return (
+        <>
+        <img src="/logo.png" alt="A Server Currusponding Image"/>
+        <h1>Welcome to this NstxJS Course!</h1>
+        </>
+    )
+}
