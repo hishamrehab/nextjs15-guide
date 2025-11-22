c908be1aa72648c59f331e08fc2a8e09c6874bbb
hishamrehab <hishamrehab30@gmail.com>
2025-11-22 13:05:37 +0200

    NotFound Page

---
+++ 1-NextJS Essentials (App Router)/2-onwards-foodies/app/not-found.js
@@ -0,0 +1,6 @@
+export default function NotFound() {
+    return <main className="not-found">
+        <h1>Not found</h1>
+        <p>Unfrotunately, we could not find requested page or resource.</p>
+    </main>
+}
